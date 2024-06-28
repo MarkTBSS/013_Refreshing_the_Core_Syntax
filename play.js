763d3fff093252bf4f4a2adc1b4b75cbf0910460
MARK\pirat <marktbss@gmail.com>
2024-06-28 10:32:22 +0700

    [ADD] First Commit

---
+++ play.js
@@ -0,0 +1,16 @@
+var nickname = 'Max';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(nickname, age, hasHobbies));
